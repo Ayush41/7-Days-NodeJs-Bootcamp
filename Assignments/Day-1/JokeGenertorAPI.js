dda6fec1f37e63073080f4eb315c88eae6928bb3
ayush41 <anonymousking41681@gmail.com>
2024-11-16 20:34:12 +0530

    joke gen

--- Assignments/Day-1/JokeGenertorAPI.js
+++ Assignments/Day-1/JokeGenertorAPI.js
@@ -1,12 +1,22 @@
 //JOKE GENERATOR WITH JOKE API
-const getJoke = ()=>{
-    https.get('https://official-joke-api.appspot.com/random_joke'),(resp)=>{
-        let data = '';
 
-        resp.on('data',(chunk)=>{
-            data += chunk;
-        });
+const https = require('https');
 
-        
-    }
+function getJoke() {
+  https.get('https://official-joke-api.appspot.com/random_joke', (resp) => {
+    let data = '';
+    
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    
+    resp.on('end', () => {
+      const joke = JSON.parse(data);
+      console.log(`${joke.setup} - ${joke.punchline}`);
+    });
+  }).on('error', (err) => {
+    console.log('Error:', err);
+  });
 }
+
+getJoke();
