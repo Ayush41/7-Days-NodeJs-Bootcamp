aae29bc2bf1ccb82d6427266eb80c3841a9aa781
ayush41 <anonymousking41681@gmail.com>
2024-12-16 16:37:51 +0530

    custom api & routes seup

--- Gemini-backend/main.js
+++ Gemini-backend/main.js
@@ -3,11 +3,24 @@ require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const express = require('express');
-const bodyparser = require(body-parser);
+const bodyparser = require('body-parser');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3030;
+
+const app = express();
+app.use(express.json());
+app.use(bodyparser.json());
+
+app.get('/',(req,res)=>{
+    res.send("WELCOME TO THE PAGE");
+});
+
+app.get('/api/text',(req,res)=>{
+    res.send("WELCOME TO TEXT PAGE");
+});
+
 
 const prompt = "who is cristiano ronaldo and some of the awards he has got! and about his lecacy so far! explain in breif";
 
@@ -20,4 +33,8 @@ const response = async ()=>{
     }
 };
 
-response();
+// response();
+
+app.listen(PORT,()=>{
+    console.log("Listening at port 3030}");
+});
