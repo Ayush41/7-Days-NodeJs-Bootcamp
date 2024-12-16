62162f1c229c6a15f377824b5a06f4d0e2c71528
ayush41 <anonymousking41681@gmail.com>
2024-12-16 16:06:53 +0530

    worked

--- Gemini-backend/main.js
+++ Gemini-backend/main.js
@@ -1,12 +1,17 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
-
+const prompt = "who is cristiano ronaldo and some of the awards he has got! and about his lecacy so far! explain in breif";
 
 const response = async ()=>{
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+    try{
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+    }catch(err){
+        console.log("Error generating content");
+    }
 };
+
+response();
